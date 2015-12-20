772da6c319d475263b272fe8c577c754591526a1
Vic Garcia <vic.garcia@outlook.com>
2015-12-20 17:21:50 -0600

    delete prior files at start of capture

--- capture.sh
+++ capture.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-bash -c "c920 -F -o -c0 | ffmpeg -r 15 -i - -r 15 -an -vcodec copy -hls_list_size 2 -hls_time 3 -hls_flags delete_segments /tmp/c920/video.m3u8" 2> /dev/null
+
+# delete any prior video segments
+rm /c920pi/*
+
+# run capture utility, pipe to ffmpeg, output to /dev/null
+bash -c "c920 -F -o -c0 | ffmpeg -r 15 -i - -r 15 -an -vcodec copy -hls_list_size 2 -hls_time 3 -hls_flags delete_segments /c920pi/video.m3u8" 2> /dev/null
